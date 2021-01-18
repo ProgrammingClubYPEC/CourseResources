6a1b29cba2031f1b9f38cbbdedee0457116460f0
allsaidX <allsaidx7@gmail.com>
2021-01-18 08:02:36 +0300

    добавлен полный тестовый метод TestMainMethood()

---
+++ NUnitTestHelloWorld/UnitTestsMainFunction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+using NUnit.Framework;
+
+using HelloWorld;
+
+namespace NUnitTestHelloWorld
+{
+    class UnitTestsMainFunction
+    {
+        [Test]
+        public void TestMainMethood()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                Program.Main(null);
+                Assert.AreEqual(sw.ToString(), "Hello World!\r\n");
+            }
+        }
+    }
+}
