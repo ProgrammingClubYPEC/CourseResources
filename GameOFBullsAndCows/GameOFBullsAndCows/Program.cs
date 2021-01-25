3f0e4999b8e8b389272e8a6df48aed3ed59bdc59
allsaidX <allsaidx7@gmail.com>
2021-01-25 21:27:55 +0300

    реализованы функции CheckBulls & CheckCows & реализована проверочная функция ввода пользователя CurrentUserInput

--- GameOFBullsAndCows/GameOFBullsAndCows/Program.cs
+++ GameOFBullsAndCows/GameOFBullsAndCows/Program.cs
@@ -10,8 +10,8 @@ using System.Threading.Tasks;
 1)Компьютер загадывает четырехзначное число (без повторений цифр). +
 
 2)Пользователь вводит число:+
-2.1)если совпадает какая-то цифра и ее позиция, программа выводит слово БЫК и цифру. 
-2.2)Если цифра есть, но позиция ее не верная, то пишет слово КОРОВА и цифру. 
+2.1)если совпадает какая-то цифра и ее позиция, программа выводит слово БЫК и цифру. +
+2.2)Если цифра есть, но позиция ее не верная, то пишет слово КОРОВА и цифру. +
 
 3)Параллельно с каждым вводом пользователя числа, программа уменьшает счетчик попыток на 1.
 */
@@ -28,14 +28,38 @@ namespace GameOFBullsAndCows
 
             RandomArray = GetRandomArray(RandomArray);
             UserInput = GetUserInput();
-
+            
+            if(CurrentUserInput(UserInput))
+            {
+                CheckBulls(RandomArray, UserInput);
+                CheckCows(RandomArray, UserInput);
+            }
 
             Console.ReadKey();
         }
 
+        static bool CurrentUserInput(string input)
+        {
+            char[] numbers = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+            // проверка на числа
+            foreach(char item in input)
+                foreach(char numb in numbers)
+                    if (item != numb)
+                        return false;
+
+            // проверка повторимость
+            for(int index = 0; index < input.Length - 1; index += 1)
+                for(int next = index + 1; next < input.Length; next += 1)
+                    if (input[index] != input[next])
+                        return false;
+
+            return true;
+        }
+
         static int[] GetRandomArray(int[] array)
         {
-            Random rand = new Random();
+            Random rand = new Random((int)DateTime.Now.Ticks);
             array[0] = rand.Next(0, 10);
 
             for (int index = 1, next = rand.Next(0, 10); index < array.Length; next = rand.Next(0, 10))
@@ -62,6 +86,25 @@ namespace GameOFBullsAndCows
             }
 
             return input;
+        }
+
+        static void CheckBulls(int[] randArr, string input)
+        {
+            for(int index = 0; index < input.Length; index += 1)
+            {
+                if(randArr[index] == (Convert.ToInt32(input[index]) - 48))
+                    Console.WriteLine($"БЫК {randArr[index]}");
+            }
+        }
+
+        static void CheckCows(int[] randArr, string input)
+        {
+            for (int indexB = 0; indexB < input.Length; indexB += 1)
+                for (int indexA = 0; indexA < randArr.Length; indexA += 1)
+                {
+                    if (randArr[indexA] == (Convert.ToInt32(input[indexB]) - 48))
+                        Console.WriteLine($"КОРОВА {randArr[indexA]}");
+                }
         }
     }
 }
